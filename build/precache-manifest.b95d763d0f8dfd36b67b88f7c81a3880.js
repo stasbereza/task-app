d9b284e2f4cee2df12f7e3957539f891be0940b3
Stanislav <stanislav.bereza@gmail.com>
2019-04-22 00:39:30 +0300

    added homepage to package.json

---
+++ build/precache-manifest.b95d763d0f8dfd36b67b88f7c81a3880.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be90ee2df5f16e6bf233",
+    "url": "/task-manager-app/build/static/js/runtime~main.4c27b5a7.js"
+  },
+  {
+    "revision": "7c824e98dc4f49f155af",
+    "url": "/task-manager-app/build/static/js/main.8721b6e5.chunk.js"
+  },
+  {
+    "revision": "1018274e225f424e34dc",
+    "url": "/task-manager-app/build/static/js/2.1227ead3.chunk.js"
+  },
+  {
+    "revision": "7c824e98dc4f49f155af",
+    "url": "/task-manager-app/build/static/css/main.fab9d8f3.chunk.css"
+  },
+  {
+    "revision": "08641c5215c25d4849b4487867d85af5",
+    "url": "/task-manager-app/build/index.html"
+  }
+];
